f752e47198277088912925032b4725436867ffe9
Gyeonghun Park <gp112795@gmail.com>
2020-11-10 11:35:28 -0500

    Adding the Icons

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useLayoutEffect } from "react";
+import React, { useLayoutEffect } from "react";
 import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
 import Favs from "../screens/Favs";
+import { Platform } from "react-native";
 
 const Tabs = createBottomTabNavigator();
 
@@ -16,7 +18,40 @@ export default ({ navigation, route }) => {
     });
   }, [route]);
   return (
-    <Tabs.Navigator>
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          let iconName = Platform.OS === "ios" ? "ios-" : "md-";
+          switch (route.name) {
+            case "Movies":
+              iconName += "film";
+              break;
+            case "TV":
+              iconName += "tv";
+              break;
+            case "Search":
+              iconName += "search";
+              break;
+            case "Favourites":
+              iconName += "heart";
+          }
+          return (
+            <Ionicons
+              name={iconName}
+              color={focused ? "white" : "grey"}
+              size={26}
+            />
+          );
+        },
+      })}
+      tabBarOptions={{
+        showLabel: false,
+        style: {
+          backgroundColor: "black",
+          borderTopColor: "black",
+        },
+      }}
+    >
       <Tabs.Screen name="Movies" component={Movies} />
       <Tabs.Screen name="TV" component={Tv} />
       <Tabs.Screen name="Search" component={Search} />
