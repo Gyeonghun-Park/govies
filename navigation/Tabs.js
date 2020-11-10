7936c0051f21670ee775e3623ee21bf0be45648a
Gyeonghun Park <gp112795@gmail.com>
2020-11-10 00:13:46 -0500

    Updating Header

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useLayoutEffect } from "react";
+import { getFocusedRouteNameFromRoute } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Movies from "../screens/Movies";
 import Tv from "../screens/Tv";
@@ -7,11 +8,18 @@ import Favs from "../screens/Favs";
 
 const Tabs = createBottomTabNavigator();
 
-export default () => (
-  <Tabs.Navigator>
-    <Tabs.Screen name="Movies" component={Movies} />
-    <Tabs.Screen name="Tv" component={Tv} />
-    <Tabs.Screen name="Search" component={Search} />
-    <Tabs.Screen name="Favs" component={Favs} />
-  </Tabs.Navigator>
-);
+export default ({ navigation, route }) => {
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: getFocusedRouteNameFromRoute(route) ?? "Movies",
+    });
+  }, [route]);
+  return (
+    <Tabs.Navigator>
+      <Tabs.Screen name="Movies" component={Movies} />
+      <Tabs.Screen name="TV" component={Tv} />
+      <Tabs.Screen name="Search" component={Search} />
+      <Tabs.Screen name="Favourites" component={Favs} />
+    </Tabs.Navigator>
+  );
+};
