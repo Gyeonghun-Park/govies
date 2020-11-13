10ef1bcf031f9bf685560afff8984cf21047e456
Gyeonghun Park <gp112795@gmail.com>
2020-11-13 15:12:38 -0500

    Changing Card on Top

--- screens/Favs/FavsPresenter.js
+++ screens/Favs/FavsPresenter.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { PanResponder, Dimensions, Animated } from "react-native";
 import styled from "styled-components/native";
 import { apiImage } from "../../api";
@@ -18,48 +18,73 @@ const Poster = styled.Image`
 `;
 
 const styles = {
-  top: 80,
+  top: 50,
   height: HEIGHT / 1.5,
   width: "90%",
-  position: "absolute"
+  position: "absolute",
 };
 
 export default ({ results }) => {
   const [topIndex, setTopIndex] = useState(0);
+  const nextCard = () => setTopIndex((currentValue) => currentValue + 1);
   const position = new Animated.ValueXY();
   const panResponder = PanResponder.create({
     onStartShouldSetPanResponder: () => true,
     onPanResponderMove: (evt, { dx, dy }) => {
       position.setValue({ x: dx, y: dy });
     },
-    onPanResponderRelease: () => {
-      Animated.spring(position, {
-        toValue: {
-          x: 0,
-          y: 0
-        }
-      }).start();
-    }
+    onPanResponderRelease: (evt, { dx, dy }) => {
+      if (dx >= 250) {
+        Animated.spring(position, {
+          toValue: {
+            x: WIDTH + 100,
+            y: dy,
+          },
+          useNativeDriver: true,
+        }).start(nextCard);
+      } else if (dx <= -250) {
+        Animated.spring(position, {
+          toValue: {
+            x: -WIDTH - 100,
+            y: dy,
+          },
+          useNativeDriver: true,
+        }).start(nextCard);
+      } else {
+        Animated.spring(position, {
+          toValue: {
+            x: 0,
+            y: 0,
+          },
+          useNativeDriver: true,
+        }).start();
+      }
+    },
   });
   const roationValues = position.x.interpolate({
     inputRange: [-255, 0, 255],
     outputRange: ["-8deg", "0deg", "8deg"],
-    extrapolate: "clamp"
+    extrapolate: "clamp",
   });
   const secondCardOpacity = position.x.interpolate({
     inputRange: [-255, 0, 255],
     outputRange: [1, 0.2, 1],
-    extrapolate: "clamp"
+    extrapolate: "clamp",
   });
   const secondCardScale = position.x.interpolate({
     inputRange: [-255, 0, 255],
     outputRange: [1, 0.8, 1],
-    extrapolate: "clamp"
+    extrapolate: "clamp",
   });
+  useEffect(() => {
+    position.setValue({ x: 0, y: 0 });
+  }, [topIndex]);
   return (
     <Container>
       {results.map((result, index) => {
-        if (index === topIndex) {
+        if (index < topIndex) {
+          return null;
+        } else if (index === topIndex) {
           return (
             <Animated.View
               style={{
@@ -67,8 +92,8 @@ export default ({ results }) => {
                 zIndex: 1,
                 transform: [
                   { rotate: roationValues },
-                  ...position.getTranslateTransform()
-                ]
+                  ...position.getTranslateTransform(),
+                ],
               }}
               key={result.id}
               {...panResponder.panHandlers}
@@ -83,7 +108,7 @@ export default ({ results }) => {
                 ...styles,
                 zIndex: -index,
                 opacity: secondCardOpacity,
-                transform: [{ scale: secondCardScale }]
+                transform: [{ scale: secondCardScale }],
               }}
               key={result.id}
               {...panResponder.panHandlers}
@@ -97,7 +122,7 @@ export default ({ results }) => {
               style={{
                 ...styles,
                 zIndex: -index,
-                opacity: 0
+                opacity: 0,
               }}
               key={result.id}
               {...panResponder.panHandlers}
